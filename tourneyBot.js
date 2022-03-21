cc14d873a751eda9f449e695135428590b960c1e
tawan475 <43491902+tawan475@users.noreply.github.com>
2022-03-21 10:25:47 +0700

    update to v0.1.0

--- tourneyBot.js
+++ tourneyBot.js
@@ -1,29 +1,25 @@
 require('dotenv').config();
-const { banchoClient, banchoLobby } = require('@tawan475/bancho.js');
+const { banchoClient } = require('@tawan475/bancho.js');
 const bancho = new banchoClient();
 
-bancho.once('error', (err) => {
+bancho.on('error', (err) => {
     console.error(err);
 });
 
 bancho.once('ready', () => {
     console.log('Connected and logged in to bancho.');
-    bancho.join("#mp_98947336"); // test lobby
-    setTimeout(() => {
-        bancho.send("PART #mp_98947336"); // leave lobby
-    }, 6000)
 });
 
 bancho.on('disconnect', () => {
     console.log('Disconnected from bancho.');
 });
 
-bancho.on('lobbyJoined', (lobby, err) => {
-    if (err){
-        console.log(err);
-        return;
-    }
-    console.log(`Joined lobby ${lobby.channel}`);
+bancho.on('lobbyJoin', (lobby) => {
+    console.log(`Joined lobby ${lobby.channel.name}`);
+});
+
+bancho.on('lobbyLeave', (lobby) => {
+    console.log(`Left lobby ${lobby.channel.name}`);
 });
 
 bancho.on('message', (message) => {
