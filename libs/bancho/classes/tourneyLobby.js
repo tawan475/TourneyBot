2eb45584463388ae3ff83b9515d523e90a562f49
tawan475 <43491902+tawan475@users.noreply.github.com>
2022-04-01 20:58:17 +0700

    make a class

---
+++ libs/bancho/classes/tourneyLobby.js
@@ -0,0 +1,53 @@
+module.exports = class tourneyLobby {
+    constructor(bancho, channel, acronym, mappool){
+        this.bancho = bancho;
+        this.channel = channel;
+        this.acronym = acronym.toUpperCase();
+        this.mappool = mappool;
+
+        let players = await this.channel.getPlayers();
+
+        this.playerOneUsername = players.filter(p => p.slot === 0)[0].username;
+        this.playerOneUsername = players.filter(p => p.slot === 1)[0].username;
+
+        // lock the mp so players cant move to change team
+        // first slot will always be blue
+        // second slot will always be red
+        this.channel.send(`!mp lock`);
+        // set mp to TeamVS ScoreV2 with 2 slots
+        this.channel.send(`!mp set 2 3 2`);
+
+        this.channel.send("Tourney match created!");
+
+        if (this.playerOneUsername && this.playerOneUsername) {
+            this.channel.send(`!mp name ${this.acronym}: (${playerOneUsername}) vs (${playerTwoUsername})`);
+        }
+
+        const listener = (type, player) => {
+            if (type === "leave"){
+                if (player === this.playerOneUsername) this.playerOneUsername = null;
+                if (player === this.playerOneUsername) this.playerOneUsername = null;
+                return;
+            }
+            if (player.slot === 0) this.playerOneUsername = player.username;
+            if (player.slot === 1) this.playerOneUsername = player.username;
+            if (this.playerOneUsername && this.playerOneUsername) {
+                channel.send(`!mp name ${acronym}: (${this.playerOneUsername}) vs (${this.playerOneUsername})`);
+            }
+        }
+        this.channel.on("playerLeft", player => listener("leave", player));
+        this.channel.on("playerJoined", player => listener("join", player));
+        this.channel.on("playerMoved", player => listener("move", player));
+        const channelLeaveListener = (destination) => {
+            if (destination === this.channel.name) {
+                this.channel.removeListener("playerLeft", listener);
+                this.channel.removeListener("playerJoin", listener);
+                this.channel.removeListener("playerMoved", listener);
+                this.bancho.removeListener("channelLeave", channelLeaveListener);
+            }
+        }
+        this.bancho.on("channelLeave", channelLeaveListener);
+    }
+
+
+}
--- libs/bancho/commands/makeTourney.js
+++ libs/bancho/commands/makeTourney.js
@@ -1,3 +1,5 @@
+const tourneyLobby = require('../casses/tourneyLobby.js');
+
 module.exports = function (bancho) {
     this.log = bancho.log.dir("makeTourney.js");
     module.log = this.log
@@ -11,57 +13,10 @@ module.exports = function (bancho) {
         if (bancho.tourneyLobby.has(message.channel.name)) return channel.send(`This match is already been marked as a tournament lobby!`);
         module.log(`Making tourney for ${message.channel.name}`);
 
+        let lobby = new tourneyLobby(bancho, message.channel, bancho.app.acronym, bancho.app.mappool);
+
         // add this channel to tourney lobbies
-        bancho.tourneyLobby.set(message.channel.name, message.channel)
-
-        // remove everyone from the multiplayer
-        let players = await message.channel.getPlayers();
-        let channel = message.channel;
-        // channel.send(`!mp size 1`)
-        // let firstPlayer = players.filter(p => p.slot === 1)[0];
-        // if (firstPlayer && firstPlayer.username) channel.send(`!mp kick ${firstPlayer.username}`);
-
-        let playerOneUsername = players.filter(p => p.slot === 0)[0].username;
-        let playerTwoUsername = players.filter(p => p.slot === 1)[0].username;
-
-        // lock the mp so players cant move to change team
-        // first slot will always be blue
-        // second slot will always be red
-        channel.send(`!mp lock`);
-        // set mp to TeamVS ScoreV2 with 2 slots
-        channel.send(`!mp set 2 3 2`);
-        let acronym = bancho.app.acronym.toUpperCase();
-
-        channel.send("Tourney match created!");
-
-        if (playerOneUsername && playerTwoUsername) {
-            channel.send(`!mp name ${acronym}: (${playerOneUsername}) vs (${playerTwoUsername})`);
-        }
-
-        const listener = (type, player) => {
-            if (type === "leave"){
-                if (player === playerOneUsername) playerOneUsername = null;
-                if (player === playerTwoUsername) playerTwoUsername = null;
-                return;
-            }
-            if (player.slot === 0) playerOneUsername = player.username;
-            if (player.slot === 1) playerTwoUsername = player.username;
-            if (playerOneUsername && playerTwoUsername) {
-                channel.send(`!mp name ${acronym}: (${playerOneUsername}) vs (${playerTwoUsername})`);
-            }
-        }
-        channel.on("playerLeft", player => listener("leave", player));
-        channel.on("playerJoined", player => listener("join", player));
-        channel.on("playerMoved", player => listener("move", player));
-        const channelLeaveListener = (destination) => {
-            if (destination === channel.name) {
-                channel.removeListener("playerLeft", listener);
-                channel.removeListener("playerJoin", listener);
-                channel.removeListener("playerMoved", listener);
-                bancho.removeListener("channelLeave", channelLeaveListener);
-            }
-        }
-        bancho.on("channelLeave", channelLeaveListener);
+        bancho.tourneyLobby.set(message.channel.name, lobby);
     };
 
     return module;
